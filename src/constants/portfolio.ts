7fa67869f0416ca6c123539a89be0009e2303c14
Akshitwadhwa <akshitwadhwa06@gmail.com>
2025-11-06 22:19:33 +0530

    Changing css tittle

--- src/constants/portfolio.ts
+++ src/constants/portfolio.ts
@@ -13,7 +13,7 @@ export const PORTFOLIO_CONFIG: PortfolioConfig = {
     {
       variant: 'green', 
       icon: 'download',
-      text: 'Download Resume',
+      text: 'View Resume',
       href: 'https://drive.google.com/file/d/YOUR_FILE_ID/view?usp=sharing' // Replace YOUR_FILE_ID with your actual Google Drive file ID
     },
     
